d820065fde0c4dc127a426238f9f5836dc2ba5e4
Loren Baum <lorencalebbaum@cedarville.edu>
2018-10-29 14:53:18 -0400

    Finish cmd4

--- working/hw4_mod.c
+++ working/hw4_mod.c
@@ -1,5 +1,5 @@
 /*
- * scull.c -- the bare scull char module
+ * hw4mod.c -- the bare hw4mod char module
  *
  * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
  * Copyright (C) 2001 O'Reilly & Associates
@@ -38,40 +38,40 @@
 /*
  * Our parameters which can be set at load time.
  */
-int scull_major   = SCULL_MAJOR;
-int scull_minor   = 0;
-int scull_nr_devs = SCULL_NR_DEVS;
-int scull_quantum = SCULL_QUANTUM;
-int scull_qset    = SCULL_QSET;
+int hw4mod_major   = HW4MOD_MAJOR;
+int hw4mod_minor   = 0;
+int hw4mod_nr_devs = HW4MOD_NR_DEVS;
+int hw4mod_quantum = HW4MOD_QUANTUM;
+int hw4mod_qset    = HW4MOD_QSET;
 
-module_param(scull_major,   int, S_IRUGO);
-module_param(scull_minor,   int, S_IRUGO);
-module_param(scull_nr_devs, int, S_IRUGO);
-module_param(scull_quantum, int, S_IRUGO);
-module_param(scull_qset,    int, S_IRUGO);
+module_param(hw4mod_major,   int, S_IRUGO);
+module_param(hw4mod_minor,   int, S_IRUGO);
+module_param(hw4mod_nr_devs, int, S_IRUGO);
+module_param(hw4mod_quantum, int, S_IRUGO);
+module_param(hw4mod_qset,    int, S_IRUGO);
 
 MODULE_AUTHOR("Alessandro Rubini, Jonathan Corbet modified K. Shomper");
 MODULE_LICENSE("Dual BSD/GPL");
 
-/* the set of devices allocated in scull_init_module */
-struct scull_dev *scull_devices = NULL;
+/* the set of devices allocated in hw4mod_init_module */
+struct hw4mod_dev *hw4mod_devices = NULL;
 
 /*
  * Open: to open the device is to initialize it for the remaining methods.
  */
-int scull_open(struct inode *inode, struct file *filp) {
+int hw4mod_open(struct inode *inode, struct file *filp) {
 
-   /* the device this function is handling (one of the scull_devices) */
-   struct scull_dev *dev;
+   /* the device this function is handling (one of the hw4mod_devices) */
+   struct hw4mod_dev *dev;
 
-   /* we need the scull_dev object (dev), but the required prototpye
+   /* we need the hw4mod_dev object (dev), but the required prototpye
       for the open method is that it receives a pointer to an inode.
       now an inode contains a struct cdev (the field is called
       i_cdev) and we can use this field with the container_of macro
-      to obtain the scull_dev object (since scull_dev also contains
+      to obtain the hw4mod_dev object (since hw4mod_dev also contains
       a cdev object.
     */
-   dev = container_of(inode->i_cdev, struct scull_dev, cdev);
+   dev = container_of(inode->i_cdev, struct hw4mod_dev, cdev);
 
    /* so that we don't need to use the container_of() macro repeatedly,
       we save the handle to dev in the file's private_data for other methods.
@@ -91,11 +91,11 @@ int scull_open(struct inode *inode, struct file *filp) {
 
 /*
  * Release: release is the opposite of open, so it deallocates any
- *          memory allocated by scull_open and shuts down the device.
+ *          memory allocated by hw4mod_open and shuts down the device.
  *          since open didn't allocate anything and our device exists
  *          only in memory, there are no actions to take here.
  */
-int scull_release(struct inode *inode, struct file *filp) {
+int hw4mod_release(struct inode *inode, struct file *filp) {
    return 0;
 }
 
@@ -107,10 +107,10 @@ int scull_release(struct inode *inode, struct file *filp) {
  *       that indicates that buf originates from user space memory and
  *       should therefore not be trusted.
  */
-ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+ssize_t hw4mod_read(struct file *filp, char __user *buf, size_t count,
                     loff_t *f_pos) {
 
-   struct scull_dev  *dev  = filp->private_data; 
+   struct hw4mod_dev  *dev  = filp->private_data; 
    ssize_t retval   = 0;
 
    /* acquire the semaphore */
@@ -132,10 +132,10 @@ ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
  *        file position f_pos.  The attribute "__user" indicates that buf 
  *        originates from user space memory and should therefore not be trusted.
  */
-ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
+ssize_t hw4mod_write(struct file *filp, const char __user *buf, size_t count,
                      loff_t *f_pos) {
 
-   struct scull_dev  *dev  = filp->private_data;
+   struct hw4mod_dev  *dev  = filp->private_data;
    ssize_t retval   = -ENOMEM;         /* value used in "goto out" statements */
 
    if (down_interruptible(&dev->sem)) return -ERESTARTSYS;
@@ -150,17 +150,10 @@ ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
  *         call.  It accomplishes this via a command value and an arg
  *         parameter which indicates which action to take.
  */
-long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+long hw4mod_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
 
    int err    = 0, tmp;
    int retval = 0;
-    
-   /*
-    * extract the type and number bitfields, and don't decode
-    * wrong cmds: return ENOTTY (inappropriate ioctl) before access_ok()
-    */
-   if (_IOC_TYPE(cmd) != SCULL_IOC_MAGIC) return -ENOTTY;
-   if (_IOC_NR(cmd)   >  SCULL_IOC_MAXNR) return -ENOTTY;
 
    /*
     * the direction is a bitmask, and VERIFY_WRITE catches R/W
@@ -168,99 +161,15 @@ long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
     * access_ok is kernel-oriented, so the concept of "read" and
     * "write" is reversed
     */
-   if (_IOC_DIR(cmd) & _IOC_READ) {
-      err = !access_ok(VERIFY_WRITE, (void __user *)arg, _IOC_SIZE(cmd));
-   } else if (_IOC_DIR(cmd) & _IOC_WRITE) {
-      err = !access_ok(VERIFY_READ,  (void __user *)arg, _IOC_SIZE(cmd));
-   }
-
-   /* exit on error */
-   if (err) return -EFAULT;
 
    /* parse the incoming command */
    switch(cmd) {
 
-      /* Reset: values are compile-time defines */
-     case SCULL_IOCRESET:
-      scull_quantum = SCULL_QUANTUM;
-      scull_qset    = SCULL_QSET;
-      break;
-        
-      /* Set: arg points to the value */
-     case SCULL_IOCSQUANTUM:
+     case HW4MOD_IOCSQSET:
         if (! capable (CAP_SYS_ADMIN))
            return -EPERM;
-        retval = __get_user(scull_quantum, (int __user *)arg);
+        retval = __get_user(hw4mod_qset, (int __user *)arg);
         break;
-
-      /* Tell: arg is the value */
-     case SCULL_IOCTQUANTUM:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        scull_quantum = arg;
-        break;
-
-      /* Get: arg is pointer to result */
-     case SCULL_IOCGQUANTUM:
-        retval = __put_user(scull_quantum, (int __user *)arg);
-        break;
-
-     /* Query: return it (it's positive) */
-     case SCULL_IOCQQUANTUM:
-        return scull_quantum;
-
-     /* eXchange: use arg as pointer; requires user to have root privilege */
-     case SCULL_IOCXQUANTUM:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        tmp = scull_quantum;
-        retval = __get_user(scull_quantum, (int __user *)arg);
-        if (retval == 0)
-           retval = __put_user(tmp, (int __user *)arg);
-        break;
-
-     /* sHift: like Tell + Query; also requires root access */
-     case SCULL_IOCHQUANTUM:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        tmp = scull_quantum;
-        scull_quantum = arg;
-        return tmp;
-        
-     case SCULL_IOCSQSET:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        retval = __get_user(scull_qset, (int __user *)arg);
-        break;
-
-     case SCULL_IOCTQSET:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        scull_qset = arg;
-        break;
-
-     case SCULL_IOCGQSET:
-        retval = __put_user(scull_qset, (int __user *)arg);
-        break;
-
-     case SCULL_IOCQQSET:
-        return scull_qset;
-
-     case SCULL_IOCXQSET:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        tmp = scull_qset;
-        retval = __get_user(scull_qset, (int __user *)arg);
-        if (retval == 0)
-           retval = put_user(tmp, (int __user *)arg);
-        break;
-
-     case SCULL_IOCHQSET:
-        if (! capable (CAP_SYS_ADMIN))
-           return -EPERM;
-        tmp = scull_qset;
-        scull_qset = arg;
-        return tmp;
 
      /* redundant, as cmd was checked against MAXNR */
      default:
@@ -273,11 +182,11 @@ long scull_ioctl(struct file *filp, unsigned int cmd, unsigned long arg) {
 
 
 /*
- * Seek:  the only one of the "extended" operations which scull implements.
+ * Seek:  the only one of the "extended" operations which hw4mod implements.
  */
-loff_t scull_llseek(struct file *filp, loff_t off, int whence) {
+loff_t hw4mod_llseek(struct file *filp, loff_t off, int whence) {
 
-   struct scull_dev *dev    = filp->private_data;
+   struct hw4mod_dev *dev    = filp->private_data;
    loff_t            newpos;
 
    /* reset the file position as is standard */
@@ -298,9 +207,6 @@ loff_t scull_llseek(struct file *filp, loff_t off, int whence) {
       return -EINVAL;
    }
 
-   /* file positions can't be negative */
-   if (newpos < 0) return -EINVAL;
-
    /* set the postion and return */
    filp->f_pos = newpos;
    return newpos;
@@ -309,14 +215,14 @@ loff_t scull_llseek(struct file *filp, loff_t off, int whence) {
 /* this assignment is what "binds" the template file operations with those that
  * are implemented herein.
  */
-struct file_operations scull_fops = {
+struct file_operations hw4mod_fops = {
    .owner =    THIS_MODULE,
-   .llseek =   scull_llseek,
-   .read =     scull_read,
-   .write =    scull_write,
-   .unlocked_ioctl = scull_ioctl,
-   .open =     scull_open,
-   .release =  scull_release,
+   .llseek =   hw4mod_llseek,
+   .read =     hw4mod_read,
+   .write =    hw4mod_write,
+   .unlocked_ioctl = hw4mod_ioctl,
+   .open =     hw4mod_open,
+   .release =  hw4mod_release,
 };
 
 /*
@@ -328,54 +234,54 @@ struct file_operations scull_fops = {
  * Thefore, it must be careful to work correctly even if some of the items
  * have not been initialized
  */
-void scull_cleanup_module(void) {
+void hw4mod_cleanup_module(void) {
 
-   dev_t devno = MKDEV(scull_major, scull_minor);
+   dev_t devno = MKDEV(hw4mod_major, hw4mod_minor);
 
    /* if the devices were succesfully allocated, then the referencing pointer
     * will be non-NULL.
     */
-   if (scull_devices != NULL) {
+   if (hw4mod_devices != NULL) {
 
       /* Get rid of our char dev entries by first deallocating memory and then
        * deleting them from the kernel */
       int i;
-      for (i = 0; i < scull_nr_devs; i++) {
-         scull_trim(scull_devices + i);
-         cdev_del(&scull_devices[i].cdev);
+      for (i = 0; i < hw4mod_nr_devs; i++) {
+	 finalize_vault(&(hw4mod_devices+i)->pwd_vault);
+         cdev_del(&hw4mod_devices[i].cdev);
       }
 
       /* free the referencing structures */
-      kfree(scull_devices);
+      kfree(hw4mod_devices);
    }
 
    /* cleanup_module is never called if registering failed */
-   unregister_chrdev_region(devno, scull_nr_devs);
+   unregister_chrdev_region(devno, hw4mod_nr_devs);
 
-   printk(KERN_NOTICE "scull unloaded\n");
+   printk(KERN_NOTICE "hw4mod unloaded\n");
 }
 
 
 /*
  * Set up the char_dev structure for this device.
  */
-static void scull_setup_cdev(struct scull_dev *dev, int index) {
-   int err, devno = MKDEV(scull_major, scull_minor + index);
+static void hw4mod_setup_cdev(struct hw4mod_dev *dev, int index) {
+   int err, devno = MKDEV(hw4mod_major, hw4mod_minor + index);
     
    /* cdev_init() and cdev_add() are kernel-required initialization */
-   cdev_init(&dev->cdev, &scull_fops);
+   cdev_init(&dev->cdev, &hw4mod_fops);
    dev->cdev.owner = THIS_MODULE;
-   dev->cdev.ops   = &scull_fops;
+   dev->cdev.ops   = &hw4mod_fops;
    err             = cdev_add (&dev->cdev, devno, 1);
 
    /* Fail gracefully if need be */
-   if (err) printk(KERN_NOTICE "Error %d adding scull%d", err, index);
+   if (err) printk(KERN_NOTICE "Error %d adding hw4mod%d", err, index);
 
-   printk(KERN_NOTICE "scull device setup\n");
+   printk(KERN_NOTICE "hw4mod device setup\n");
 }
 
 
-int scull_init_module(void) {
+int hw4mod_init_module(void) {
    int result, i;
    dev_t dev = 0;
 
@@ -383,17 +289,17 @@ int scull_init_module(void) {
     * Compile-time default for major is zero (dynamically assigned) unless 
     * directed otherwise at load time.  Also get range of minors to work with.
     */
-   if (scull_major == 0) {
-      result      = alloc_chrdev_region(&dev,scull_minor,scull_nr_devs,"scull");
-      scull_major = MAJOR(dev);
+   if (hw4mod_major == 0) {
+      result      = alloc_chrdev_region(&dev,hw4mod_minor,hw4mod_nr_devs,"hw4mod");
+      hw4mod_major = MAJOR(dev);
    } else {
-      dev    = MKDEV(scull_major, scull_minor);
-      result = register_chrdev_region(dev, scull_nr_devs, "scull");
+      dev    = MKDEV(hw4mod_major, hw4mod_minor);
+      result = register_chrdev_region(dev, hw4mod_nr_devs, "hw4mod");
    }
 
    /* report failue to aquire major number */
    if (result < 0) {
-      printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+      printk(KERN_WARNING "hw4mod: can't get major %d\n", hw4mod_major);
       return result;
    }
 
@@ -401,38 +307,37 @@ int scull_init_module(void) {
     * allocate the devices -- we can't have them static, as the number
     * can be specified at load time
     */
-   scull_devices = kmalloc(scull_nr_devs*sizeof(struct scull_dev), GFP_KERNEL);
+   hw4mod_devices = kmalloc(hw4mod_nr_devs*sizeof(struct hw4mod_dev), GFP_KERNEL);
 
    /* exit if memory allocation fails */
-   if (!scull_devices) {
+   if (!hw4mod_devices) {
       result = -ENOMEM;
       goto fail;
    }
 
    /* otherwise, zero the memory */
-   memset(scull_devices, 0, scull_nr_devs * sizeof(struct scull_dev));
+   memset(hw4mod_devices, 0, hw4mod_nr_devs * sizeof(struct hw4mod_dev));
 
    /* Initialize each device. */
-   for (i = 0; i < scull_nr_devs; i++) {
-      scull_devices[i].quantum = scull_quantum;
-      scull_devices[i].qset    = scull_qset;
-      sema_init(&scull_devices[i].sem, 1);
-      scull_setup_cdev(&scull_devices[i], i);
+   for (i = 0; i < hw4mod_nr_devs; i++) {
+      initialize_vault(&hw4mod_devices[i].pwd_vault, HW4MOD_MAX_USERS_IN_VAULT);
+      sema_init(&hw4mod_devices[i].sem, 1);
+      hw4mod_setup_cdev(&hw4mod_devices[i], i);
    }
 
-   printk(KERN_NOTICE "scull loaded\n");
+   printk(KERN_NOTICE "hw4mod loaded\n");
 
    /* succeed */
    return 0;
 
    /* failure, so cleanup is necessary */
   fail:
-   scull_cleanup_module();
+   hw4mod_cleanup_module();
    return result;
 }
 
 /* identify to the kernel the entry points for initialization and release, these
  * functions are called on insmod and rmmod, respectively
  */
-module_init(scull_init_module);
-module_exit(scull_cleanup_module);
+module_init(hw4mod_init_module);
+module_exit(hw4mod_cleanup_module);
